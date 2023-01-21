40d379661ec3c20f183c4d4db4642cee58a46cec
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-21 22:37:46 +0700

    Deleted Space

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
-
+const host ='localhost';
+const port =3000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
-
-const host ='localhost';
-const port =3001;
-
 app.listen(port, host, () => {
   console.log(`Server berjalan di http://${host}:${port}`);
 });
--- models/UserModel.js
+++ models/UserModel.js
@@ -2,7 +2,7 @@ import Sequelize from "sequelize";
 import db from "../config/Database.js";
 
 const User = db.define("user", {
-  name: {
+  nama: {
     type: Sequelize.STRING,
     allowNull: false,
   },
