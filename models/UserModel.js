194b135f5de2272cc2707f092fbb455fb49f4827
Daffa Fakhuddin Arrozy <daffatgi02@gmail.com>
2023-01-24 20:51:17 +0700

    Update DB user

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,12 +1,12 @@
 import Sequelize from "sequelize";
 import db from "../config/Database.js";
 
-const User = db.define("user", {
+const User = db.define("users", {
   nim: {
     type: Sequelize.STRING,
     allowNull: false,
     validate: {
-      is: /^[0-9]*$/  //pakai regex karena NIM tidak boleh text
+      is: /^[0-9]*$/  //pakai regex karena NIM tidak bisa ditambah abjad
     }
   },
   nama: {
